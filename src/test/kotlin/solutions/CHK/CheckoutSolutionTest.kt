40748528a9b9dd4830e89f0cc1a2b12faa8af5a1
sbx_user1051 <sbx_user1051@169.254.82.165>
2024-01-28 11:09:59 +0000

    Sun Jan 28 11:09:59 UTC 2024

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -2,15 +2,25 @@ package solutions.SUM
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import solutions.CHK.CheckoutSolution
 import solutions.HLO.HelloSolution
 
-class HelloSolutionTest {
+class CheckoutSolutionTest {
 
     @Test
-    fun hello() {
-        Assertions.assertEquals("Hello, Ace!", HelloSolution.hello("Ace"))
-        Assertions.assertEquals("Hello, John!", HelloSolution.hello("John"))
+    fun checkout() {
+        Assertions.assertEquals(50, CheckoutSolution.checkout("A"))
+        Assertions.assertEquals(30, CheckoutSolution.checkout("B"))
+        Assertions.assertEquals(20, CheckoutSolution.checkout("C"))
+        Assertions.assertEquals(15, CheckoutSolution.checkout("D"))
+
+        Assertions.assertEquals(130, CheckoutSolution.checkout("AAA"))
+        Assertions.assertEquals(45, CheckoutSolution.checkout("BB"))
+        
+        Assertions.assertEquals(-1, CheckoutSolution.checkout("X"))
+        Assertions.assertEquals(-1, CheckoutSolution.checkout("A9"))
     }
 
     
 }
+
