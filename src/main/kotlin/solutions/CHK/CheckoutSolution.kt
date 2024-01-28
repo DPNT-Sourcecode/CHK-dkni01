2ec2aab267aaa08929c1c9867e85ec428cc965e6
sbx_user1051 <sbx_user1051@169.254.82.165>
2024-01-28 11:41:41 +0000

    Sun Jan 28 11:41:41 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,38 +1,31 @@
 package solutions.CHK
 
 object CheckoutSolution {
+    const val PRICE_A = 50
+    const val PRICE_B = 30
+    const val PRICE_C = 20
+    const val PRICE_D = 15
+    
     fun checkout(skus: String): Int {
         if (skus.any { !listOf('A', 'B', 'C', 'D').contains(it) }) {
             return -1
         }
-        
-        val PRICE_A = 50
-        val PRICE_B = 30
-        val PRICE_C = 20
-        val PRICE_D = 15
-        
-        var numA = skus.count { it == 'A'}
-        val numB = skus.count { it == 'B'}
-        val numC = skus.count { it == 'C'}
-        val numD = skus.count { it == 'D'}
-        
-        val numATotal = numA * PRICE_A
-        val leftoverA = numATotal % 150
-        val offerA = ((numATotal-leftoverA) / 150) * 130
-//            val newtotalA = offerA + leftoverA
-//            println("❗️ $leftoverA, $offerA, $newtotalA")
 
-        val numBTotal = numB * PRICE_B
+        val numATotal = skus.count { it == 'A' } * PRICE_A
+        val leftoverA = numATotal % 150
+        val offerA = ((numATotal - leftoverA) / 150) * 130
+
+        val numBTotal = skus.count { it == 'B' } * PRICE_B
         val leftoverB = numBTotal % 60
-        val offerB = ((numBTotal-leftoverB) / 60) * 45
+        val offerB = ((numBTotal - leftoverB) / 60) * 45
         
-        
-        val sums = (offerA + leftoverA) + (offerB + leftoverB) + (numC * PRICE_C) + (numD * PRICE_D)
-        return sums
-        
-//        return -1
+        return (offerA + leftoverA) + 
+                (offerB + leftoverB) + 
+                (skus.count { it == 'C' } * PRICE_C) + 
+                (skus.count { it == 'D' } * PRICE_D)
     }
 }
+
 
 
 
