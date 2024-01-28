51e76b7f50f449eea42e150640971ac6f99429da
sbx_user1051 <sbx_user1051@169.254.82.165>
2024-01-28 10:52:18 +0000

    Sun Jan 28 10:52:18 UTC 2024

--- src/test/kotlin/solutions/SUM/SumSolutionTest.kt
+++ src/test/kotlin/solutions/SUM/SumSolutionTest.kt
@@ -10,4 +10,11 @@ class SumSolutionTest {
         Assertions.assertEquals(2, SumSolution.sum(1, 1))
         Assertions.assertEquals(0, SumSolution.sum(0, 0))
     }
+
+    @Test
+    fun sum() {
+        Assertions.assertEquals(2, SumSolution.sum(1, 1))
+        Assertions.assertEquals(0, SumSolution.sum(0, 0))
+    }
 }
+
