db8a7ee3f2c5b9a4aa6b59149502456f3bf8a345
sbx_user1051 <sbx_user1051@169.254.82.165>
2024-01-28 11:25:00 +0000

    Sun Jan 28 11:25:00 UTC 2024

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -11,10 +11,19 @@ object CheckoutSolution {
         val PRICE_C = 20
         val PRICE_D = 15
         
-        val numA = skus.count { it == 'A'}
+        var numA = skus.count { it == 'A'}
         val numB = skus.count { it == 'B'}
         val numC = skus.count { it == 'C'}
         val numD = skus.count { it == 'D'}
+        
+        val checkAOffer = numA % 3
+        
+        println("❗️ $checkAOffer")
+        println("❗️ ${4 % 3}")
+        
+        if (numA > 3) {
+            numA = (numA - 150) + 130
+        }
         
         val sums = (numA * PRICE_A) + (numB * PRICE_B) + (numC * PRICE_C) + (numD * PRICE_D)
         return sums
@@ -22,4 +31,5 @@ object CheckoutSolution {
 //        return -1
     }
 }
+
 
