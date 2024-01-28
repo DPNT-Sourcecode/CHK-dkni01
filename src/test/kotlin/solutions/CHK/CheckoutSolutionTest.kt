26800c961d2c11af263b93fa37a9f471341c6fa0
sbx_user1051 <sbx_user1051@169.254.82.165>
2024-01-28 11:50:43 +0000

    Sun Jan 28 11:50:43 UTC 2024

--- src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
+++ src/test/kotlin/solutions/CHK/CheckoutSolutionTest.kt
@@ -13,12 +13,17 @@ class CheckoutSolutionTest {
         Assertions.assertEquals(30, CheckoutSolution.checkout("B"))
         Assertions.assertEquals(20, CheckoutSolution.checkout("C"))
         Assertions.assertEquals(15, CheckoutSolution.checkout("D"))
+//        Assertions.assertEquals(40, CheckoutSolution.checkout("E"))
         Assertions.assertEquals(145, CheckoutSolution.checkout("ABCDDD"))
 
         Assertions.assertEquals(180, CheckoutSolution.checkout("AAAA"))
-        Assertions.assertEquals(260, CheckoutSolution.checkout("AAAAAA"))
+        Assertions.assertEquals(250, CheckoutSolution.checkout("AAAAAA"))
         Assertions.assertEquals(45, CheckoutSolution.checkout("BB"))
         Assertions.assertEquals(220, CheckoutSolution.checkout("BBBBAAA"))
+        
+//        Assertions.assertEquals(80, CheckoutSolution.checkout("EEB"))
+//        Assertions.assertEquals(110, CheckoutSolution.checkout("EEBB"))
+//        Assertions.assertEquals(160, CheckoutSolution.checkout("EEEEBB"))
 //        
         Assertions.assertEquals(-1, CheckoutSolution.checkout("X"))
         Assertions.assertEquals(-1, CheckoutSolution.checkout("A9"))
@@ -26,3 +31,4 @@ class CheckoutSolutionTest {
 
     
 }
+
